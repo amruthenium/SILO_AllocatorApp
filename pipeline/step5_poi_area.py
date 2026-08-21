6a7652a923e6368733bf0c64e64cbb80889269cf
amrutha <amrutha.viswanath@simplex4data.de>
2026-08-21 16:42:32 +0200

    changed scope of gdf, id_col, perc to global

--- pipeline/step5_poi_area.py
+++ pipeline/step5_poi_area.py
@@ -34,6 +34,7 @@ def _detect_layers(gpkg_path, log):
 
 
 def _prepare_layer(gpkg_path, layer, percentage_csv, log):
+    global gdf, perc, id_col
     gdf[id_col] = gdf[id_col].astype(str) #forces both to the sametype or else merge fails and concat usage is a bit clunky
     perc[id_col] = perc[id_col].astype(str)
     gdf = gpd.read_file(gpkg_path, layer=layer) 
